afb07f8588a758599dec3f2b0e0d7ce9f2e76514
jeffw <jeffw@WIN-LAP>
2021-09-06 21:20:37 +0200

    Add project files.

---
+++ OdoToFood.Data/DomainClasses/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Data.DomainClasses
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
